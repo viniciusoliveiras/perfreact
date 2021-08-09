b0f96d05b581f084b8cecd026d249798a4f817bc
viniciusoliveiras <vinitag190@gmail.com>
2021-08-09 15:10:33 -0300

    Otimizações | Virtualização

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -17,11 +18,31 @@ export function SearchResults({
   totalPrice,
   onAddToWishList,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div className="searchResults">
       <h2>R$ {totalPrice}</h2>
 
-      {results.map((product) => {
+      <List
+        height={425}
+        rowHeight={70}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map((product) => {
         return (
           <ProductItem
             key={product.id}
@@ -29,7 +50,7 @@ export function SearchResults({
             onAddToWishList={onAddToWishList}
           />
         );
-      })}
+      })} */}
     </div>
   );
 }
